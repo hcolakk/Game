404d83cea19571bb6c52468e9f660e3b2d90695d
hcolakk <117113274+hcolakk@users.noreply.github.com>
2023-03-08 17:48:50 +0100

    son

--- info.js
+++ info.js
@@ -3,7 +3,7 @@ var count=0;
 
 function Control(){
     var i=Number(document.getElementById("in1").value);
-    if(t>1){
+    if(t>i){
         count ++;
         document.getElementById("cc").innerHTML=count;
         document.getElementById("sonuc").innerHTML="Increase the number "+t;
